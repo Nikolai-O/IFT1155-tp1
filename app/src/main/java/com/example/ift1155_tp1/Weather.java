4e19418c6476c88d3057ea2193c2e2b57dda7375
Nikolai Olekhnovitch <NOlekhnovitch@gmail.com>
2022-03-21 09:57:47 -0400

    Completed vertical layout for Pixel 2.

--- app/src/main/java/com/example/ift1155_tp1/Weather.java
+++ app/src/main/java/com/example/ift1155_tp1/Weather.java
@@ -2,6 +2,7 @@ package com.example.ift1155_tp1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +14,18 @@ public class Weather extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather);
-        TextView tv = findViewById(R.id.weatherText);
+        TextView currentTV = findViewById(R.id.currentTV);
+        TextView currentCondition = findViewById(R.id.currentCondTV);
+        TextView windDirectionTV = findViewById(R.id.windDirection);
+        TextView windSpeedTV = findViewById(R.id.windSpeed);
+        TextView summaryTV = findViewById(R.id.summaryTV);
+        TextView tmrTV = findViewById(R.id.tmrTV);
+        TextView tmrSummaryTV = findViewById(R.id.tmrSummaryTV);
+        TextView tmrTempTV = findViewById(R.id.tmrTempTV);
+        TextView afterTmrTV = findViewById(R.id.afterTmrTV);
+        TextView afterTmrSummaryTV = findViewById(R.id.afterTmrSummaryTV);
+        TextView afterTmrTempTV = findViewById(R.id.afterTmrTempTV);
+
         Intent ville = getIntent();
         String temperature = ville.getStringExtra("currentTemperature");
         String condition = ville.getStringExtra("currentCondition");
@@ -24,11 +36,26 @@ public class Weather extends AppCompatActivity {
         String tmrSummary = ville.getStringExtra("tmrSummary");
         String tmrTemp = ville.getStringExtra("tmrTemp");
         String afterTmr = ville.getStringExtra("afterTmr");
-        String afterTmrSummary = ville.getStringExtra("aferTmrSummary");
+        String afterTmrSummary = ville.getStringExtra("afterTmrSummary");
         String afterTmrTemp = ville.getStringExtra("afterTmrTemp");
-        tv.setText(tv.getText() + " " + temperature  + "\n" + condition  + "\n" + windSpeed
-                + "\n" + windDirection  + "\n" + summary  + "\n" + tmr  + "\n" + tmrTemp
-                + "\n" + tmrSummary  + "\n" + afterTmr  + "\n" + afterTmrSummary  + "\n" + afterTmrTemp);
+
+        currentTV.setText(temperature + currentTV.getText());
+        currentCondition.setText(condition);
+        windDirectionTV.setText(windDirection);
+        windSpeedTV.setText(windSpeed + windSpeedTV.getText());
+        summaryTV.setText(summary);
+        tmrTV.setText(tmr);
+        tmrSummaryTV.setText(tmrSummary);
+        tmrTempTV.setText(tmrTemp + tmrTempTV.getText());
+        afterTmrTV.setText(afterTmr);
+        afterTmrSummaryTV.setText(afterTmrSummary);
+        afterTmrTempTV.setText(afterTmrTemp + afterTmrTempTV.getText());
+
+        //Toast.makeText(this, afterTmrSummary, Toast.LENGTH_LONG).show();
+
+//        currentTV.setText(tv.getText() + " " + temperature  + "\n" + condition  + "\n" + windSpeed
+//                + "\n" + windDirection  + "\n" + summary  + "\n" + tmr  + "\n" + tmrTemp
+//                + "\n" + tmrSummary  + "\n" + afterTmr  + "\n" + afterTmrSummary  + "\n" + afterTmrTemp);
     }
 
     public void back (View v) {
