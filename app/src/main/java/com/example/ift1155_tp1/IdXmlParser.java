51a7b4932bfabbe0001dcad6ccbcbe212e88d836
Nikolai Olekhnovitch <NOlekhnovitch@gmail.com>
2022-03-31 23:40:46 -0400

    Implemented XML download for city codes, saving xml file to internal storage and almost completed IdXmlParser class.

---
+++ app/src/main/java/com/example/ift1155_tp1/IdXmlParser.java
@@ -0,0 +1,136 @@
+package com.example.ift1155_tp1;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdXmlParser {
+    private static final String ns = null;
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+
+            parser.nextTag();
+            Log.i("Ids", parser.getName());
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries= new ArrayList();
+        //parser.require(XmlPullParser.START_TAG, ns, "siteList");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("site")) {
+                //entries.add(parser.getProperty("code"));
+                //Log.i("info2", (String) parser.getProperty("code"));
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+    public static class Entry {
+        public final String code;
+        public final String nameEn;
+        public final String nameFr;
+        public final String province;
+
+        private Entry(String code, String nameEn, String nameFr, String province) {
+            this.code = code;
+            this.nameEn = nameEn;
+            this.nameFr = nameFr;
+            this.province = province;
+        }
+    }
+
+    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "site");
+        String code = parser.getAttributeValue(null, "code");;
+        String nameEn = null;
+        String nameFr = null;
+        String province = null;
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("nameEn"))
+                nameEn = readNameEn(parser);
+            else if (name.equals("nameFr"))
+                nameFr = readNameFr(parser);
+            else if (name.equals("province"))
+                province = readProvince(parser);
+            else
+                skip(parser);
+        }
+
+        return new Entry(code, nameEn, nameFr, province);
+    }
+
+    private String readNameEn(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "nameEn");
+        String nameEn = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "nameEn");
+        return nameEn;
+    }
+
+    private String readNameFr(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "nameFr");
+        String nameFr = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "nameFr");
+        return nameFr;
+    }
+
+    private String readProvince(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "provinceCode");
+        String province = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "provinceCode");
+        return province;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/example/ift1155_tp1/Provinces.java
+++ app/src/main/java/com/example/ift1155_tp1/Provinces.java
@@ -1,32 +1,54 @@
 package com.example.ift1155_tp1;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Provinces extends AppCompatActivity {
-
+    //static Handler messageHandler;
+    private StringBuffer text = null;
+    static String text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.provinces);
-
+        //openHttpConnection("https://dd.weather.gc.ca/citypage_weather/xml/siteList.xml");
         downloadXML();
-
-
+        //Bundle bundle = messageHandler.obtainMessage(1).getData();
+        //String text = bundle.getString("text");
+        //Log.i("info2", text.toString());
+        //Log.i("info2", text2);
     }
 
     public void back (View v) {
@@ -41,17 +63,16 @@ public class Provinces extends AppCompatActivity {
 
     private void downloadXML() {
         final String url = "https://dd.weather.gc.ca/citypage_weather/xml/siteList.xml";
-
-        new Thread(()->{
+        new Thread(() -> {
             int BUFFER_SIZE = 2000;
             InputStream in;
             Message msg = Message.obtain();
-            msg.what=2;
+            msg.what = 1;
             try {
                 in = openHttpConnection(url);
                 InputStreamReader isr = new InputStreamReader(in);
                 int charRead;
-                StringBuffer text = new StringBuffer();
+                text = new StringBuffer();
                 char[] inputBuffer = new char[BUFFER_SIZE];
 
                 while ((charRead = isr.read(inputBuffer)) > 0) {
@@ -59,16 +80,19 @@ public class Provinces extends AppCompatActivity {
                             String.copyValueOf(inputBuffer, 0, charRead);
                     text.append(readString);
                     inputBuffer = new char[BUFFER_SIZE];
-                    Log.i("Line", readString);
+
                 }
+                //Log.i("Line", text.toString());
                 Bundle b = new Bundle();
                 b.putString("text", text.toString());
                 msg.setData(b);
                 in.close();
+                messageHandler.sendMessage(msg);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            // messageHandler.sendMessage(msg);
+
+            //messageHandler = new CustomHandler(this);
         }).start();
     }
 
@@ -101,4 +125,90 @@ public class Provinces extends AppCompatActivity {
         }
         return in;
     }
+
+    class CustomHandler extends Handler {
+
+        private final Provinces activity;
+
+        CustomHandler(Provinces activity) {
+            super(Looper.myLooper());
+            this.activity = activity;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    Log.i("info2", "what what");
+                    text2 = msg.getData().getString("text");
+                    Log.i("info2", text2);
+                    Document doc = convertStringToXMLDocument(text2);
+                    NodeList names = doc.getElementsByTagName("nameEn");
+
+                    try {
+                        OutputStreamWriter out = new OutputStreamWriter(
+                                openFileOutput("cities.xml", 0));
+                        out.write(text2);
+                        out.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    IdXmlParser parser = new IdXmlParser();
+                    InputStream is = null;
+                    try {
+                        is = openFileInput("cities.xml");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        List entries = parser.parse(is);
+                        IdXmlParser.Entry ids = (IdXmlParser.Entry) entries.get(0);
+                        IdXmlParser.Entry ids2 = (IdXmlParser.Entry) entries.get(854);
+                        Log.i("Ids", "1");
+                        Log.i("Ids", "Info: " + ids.code + " " + ids.nameEn  + " " + ids.nameFr  + " " + ids.province);
+                        Log.i("Ids2", "2");
+                        Log.i("Ids2", "Info: " + ids2.code + " " + ids2.nameEn  + " " + ids2.nameFr  + " " + ids2.province);
+                    } catch (XmlPullParserException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Log.i("info2", String.valueOf(names.getLength()));
+            }
+            //this.activity.simpleProgressBar.setVisibility(View.GONE);
+
+        }
+
+    }
+    private static Document convertStringToXMLDocument(String xmlString)
+    {
+        //Parser that produces DOM object trees from XML content
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        //API to obtain DOM Document instance
+        DocumentBuilder builder = null;
+        try
+        {
+            //Create DocumentBuilder with default configuration
+            builder = factory.newDocumentBuilder();
+
+            //Parse the content to Document object
+            Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+            return doc;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private final Handler messageHandler = new CustomHandler(this);
 }
